e8bb62f9cf7adc30cd130ce17f9c9f04ceb58372
Toby Dodge <tobydodge65@gmail.com>
2023-11-21 19:30:58 -0330

    Cleaning up utils file

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from utils import *
 
 ui.dark_mode(True)
 
-loadEditor()
+Editor.load_editor()
 code_output = ui.log(max_lines=10).classes('w-full h-40')
 
 with ui.row():
     CPPVERSION = ui.radio({1:'C', 2:'C++'}, value=2).props('inline').set_enabled(False)
-    #compileBtn = ui.button("Compile", on_click=compile_code)
-    runBtn = ui.button("Run", on_click=lambda e: compile_code(code_output))
-    loadBtn = ui.button("Load Example", on_click=lambda e: example_code("cpp"))
+    runBtn = ui.button("Run", on_click=lambda e: Code.compile_code(code_output))
+
     
 ui.run()
--- utils.py
+++ utils.py
@@ -2,33 +2,41 @@ from nicegui import ui, client
 import os
 import subprocess
 
-async def get_code():
-    return await ui.run_javascript('editor.getValue()')
+class Code():
 
-async def example_code(lang: str):
-    with open("exampleCode/example.cpp") as file:
-        await ui.run_javascript(f'editor.setValue("{file.read()}")')
+    def __init__(self, code: str, output: ui.log):
+        self.code = code
+        self.output = output
 
-async def get_height():
-    return await ui.run_javascript('window.innerHeight;')
+    async def get_code():
+        return await ui.run_javascript('editor.getValue()')
 
-async def compile_code(output: ui.log):
-    output.clear()
-    ui.notify("Compiling code")
-    with open('c_onlinecompile.cpp', 'w+') as file:
-        code_result = await get_code()
-        file.write(code_result)
-    result = subprocess.run(['g++', 'c_onlinecompile.cpp', '-o', 'c_main'], stdout=subprocess.PIPE, text=True)
-    #ui.notify(result)
-    #output.push(f"Return code: {result.returncode}")
-    run_code(output)
+    def run_code(self):
+        result = subprocess.run(["./c_main"], stdout=subprocess.PIPE, text=True)
+        self.push(result.stdout)
+        self.push(f"returned {result.returncode}")
 
+    async def compile_code(self):
+        self.clear()
+        ui.notify("Compiling code")
+        with open('c_onlinecompile.cpp', 'w+') as file:
+            code_result = await Code.get_code()
+            file.write(code_result)
+        result = subprocess.run(['g++', 'c_onlinecompile.cpp', '-o', 'c_main'], stdout=subprocess.PIPE, text=True)
+        Code.run_code(self.output)
 
-def run_code(output: ui.log):
-    result = subprocess.run(["./c_main"], stdout=subprocess.PIPE, text=True)
-    output.push(result.stdout)
-    output.push(f"returned {result.returncode}")
+class FileStructure():
+    def __init__(self, path: str):
+        self.path = path
 
-def loadEditor():
-    codeeditor = open('codeeditor.html', 'r+').read()
-    ui.add_body_html(codeeditor)
+    def get_file_structure():
+        pass
+
+    def format_file_structure():
+        pass
+
+class Editor():
+
+    def load_editor():
+        codeeditor = open('codeeditor.html', 'r+').read()
+        ui.add_body_html(codeeditor)
