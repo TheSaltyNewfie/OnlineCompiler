80eecfe89d0367319238da31dce9920da111f911
Toby Dodge <tobydodge65@gmail.com>
2023-11-21 20:09:11 -0330

    Language switching is now working

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from utils import *
 
 ui.dark_mode(True)
 
-Editor.load_editor()
+editor = Editor(lang="python")
+editor.load_editor()
+
 code_output = ui.log(max_lines=10).classes('w-full h-40')
 
 with ui.row():
-    CPPVERSION = ui.radio({1:'C', 2:'C++'}, value=2).props('inline').set_enabled(False)
+    #CPPVERSION = ui.radio({1:'C', 2:'C++'}, value=2).props('inline').set_enabled(False)
+    LANGUAGE = ui.select(["python", "c_cpp"])
+    lang_button = ui.button("Set Language (temp)", on_click=lambda e: editor.set_lang_editor(LANGUAGE.value))
     runBtn = ui.button("Run", on_click=lambda e: Code.compile_code(code_output))
 
     
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from nicegui import ui, client
 import os
 import subprocess
 
-class Code():
+class Code:
 
     def __init__(self, code: str, output: ui.log):
         self.code = code
@@ -25,7 +25,7 @@ class Code():
         result = subprocess.run(['g++', 'c_onlinecompile.cpp', '-o', 'c_main'], stdout=subprocess.PIPE, text=True)
         Code.run_code(self.output)
 
-class FileStructure():
+class FileStructure:
     def __init__(self, path: str):
         self.path = path
 
@@ -35,8 +35,14 @@ class FileStructure():
     def format_file_structure():
         pass
 
-class Editor():
+class Editor:
 
-    def load_editor():
+    def __init__(self, lang):
+        self.lang = lang
+
+    def load_editor(self):
         codeeditor = open('codeeditor.html', 'r+').read()
         ui.add_body_html(codeeditor)
+
+    async def set_lang_editor(self, new_lang):
+        await ui.run_javascript(f'editor.session.setMode("ace/mode/{new_lang}")')
