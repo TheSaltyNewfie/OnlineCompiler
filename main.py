088767f69f5dd5c2901c60a6b3bd73763808eb59
Toby Dodge <tobydodge65@gmail.com>
2023-11-21 20:42:09 -0330

    Language switching is buggy

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ with ui.row():
     #CPPVERSION = ui.radio({1:'C', 2:'C++'}, value=2).props('inline').set_enabled(False)
     LANGUAGE = ui.select(["python", "c_cpp"])
     lang_button = ui.button("Set Language (temp)", on_click=lambda e: editor.set_lang_editor(LANGUAGE.value))
-    runBtn = ui.button("Run", on_click=lambda e: Code.compile_code(code_output))
+    runBtn = ui.button("Run", on_click=lambda e: Code.compile_code(code_output, LANGUAGE.value))
 
     
 ui.run()
--- utils.py
+++ utils.py
@@ -4,18 +4,40 @@ import subprocess
 
 class Code:
 
-    def __init__(self, code: str, output: ui.log):
-        self.code = code
+    def __init__(self, output: ui.log):
         self.output = output
 
     async def get_code():
         return await ui.run_javascript('editor.getValue()')
 
     def run_code(self):
-        result = subprocess.run(["./c_main"], stdout=subprocess.PIPE, text=True)
+        result = subprocess.run(["./workdir/c_main"], stdout=subprocess.PIPE, text=True)
         self.push(result.stdout)
         self.push(f"returned {result.returncode}")
 
+    async def compile_code(self, lang):
+        self.clear()
+        self.push("Compiling....")
+        
+        match lang:
+            # C++ Needs to be compiled, so we compile then run
+            case "c_cpp":
+                with open('workdir/c_onlinecompile.cpp', "w+") as file:
+                    code_result = await Code.get_code()
+                    file.write(code_result)
+                result = subprocess.run(['g++', 'workdir/c_onlinecompile.cpp', '-o', 'workdir/c_main'], stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+                self.push(f"{result.stderr} || {result.stdout}")
+                Code.run_code(self)
+
+            # Python only needs to be interpreted, so we wont compile this time.
+            case "python":
+                with open('workdir/python_onlinecompile.ppy', "w+") as file:
+                    code_result = await Code.get_code()
+                    file.write(code_result)
+                result = subprocess.run(['python3', 'workdir/python_onlinecompile.ppy'],stdout=subprocess.PIPE, text=True)
+                self.push(result.stdout)
+
+    '''
     async def compile_code(self):
         self.clear()
         ui.notify("Compiling code")
@@ -24,6 +46,7 @@ class Code:
             file.write(code_result)
         result = subprocess.run(['g++', 'c_onlinecompile.cpp', '-o', 'c_main'], stdout=subprocess.PIPE, text=True)
         Code.run_code(self.output)
+    '''
 
 class FileStructure:
     def __init__(self, path: str):
